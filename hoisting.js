e17f9b66deaef31b7182b3baa06e132a22736b2e
Nahomie <nducasse3@gmail.com>
2018-05-23 23:47:23 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,30 @@
+var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
-
+var thisIsCrazy = function (){
+  console.log("hey!!!")
+}
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
 }
 
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+crazy();
 
-  sayMy();
+// Part 3 //
+var name = "Kristin";
+function sayMyName() {
+  console.log(name);
 
   function sayMy() {
-    console.log(name);
-    var name = "Kristin";
+    var name = "Cricky";
   }
+  sayMy();
+
 }
